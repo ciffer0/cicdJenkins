dcd214b9a4e5b930a1febb9429ce0e5405b8b2fa
ciffer0 <schiffer00815@gmail.com>
2022-08-21 01:56:49 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Zelio
+          Learn React with Zelio the greatest
         </a>
       </header>
     </div>
