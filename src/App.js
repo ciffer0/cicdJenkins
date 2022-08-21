5213ce1d42a6b76092d216ca0e149683ac735fae
zelio <schiffer00815@gmail.com>
2022-08-21 12:57:33 +0300

    change pushed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Zelio the greatest
+          Learn React with Zelio the best
         </a>
       </header>
     </div>
